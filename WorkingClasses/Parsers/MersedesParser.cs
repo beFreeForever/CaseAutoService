06d1b18ef37f479ebe1c06831ae768ef98094966
beFreeForever <dev209210@gmail.com>
2017-04-04 11:15:44 +0300

    Add files via upload

--- WorkingClasses/Parsers/MersedesParser.cs
+++ WorkingClasses/Parsers/MersedesParser.cs
@@ -15,33 +15,43 @@ namespace SelectAutoServise.WorkingClasses.Parsers
             //List<Auto> AutoList = new List<Auto>();
             //HtmlWeb web = new HtmlWeb();
             //HtmlDocument document = web.Load("http://sales.mercedes-avilon.ru/special/goryachee-predlozhenie-table/?csref=avilon_volg:pkw:Mercedes:cpc:Google.Adwords&csref=avilon_volg:pkw:NEW:cpc:Google.Adwords&utm_source=Google.Adwords&utm_medium=cpc&utm_campaign=general_reg&utm_content=ST:g::S:::AP:::PT:1t1::P:1t1::DT:c::RI:1011981::RN:1011981::CI:743824508::GI:42576317487::PI:kwd-16822931::AI:189100468533::KW:mercedes&utm_term=mercedes&gclid=Cj0KEQjwwoLHBRDD0beVheu3lt0BEiQAvU4CKvwRMRFEZMO7i7YYpcleZ9JssEFKzmZeGcp9ZmQIZl8aAq9f8P8HAQ");
+            //List<string> s = new List<string>();
             //for (int i = 1; i <= 120; i++)
             //{
-            //    for (int j = 1; j <= 6; j++)
+            //    for (int j = 2; j <= 6; j++)
             //    {
-            //        if (j == 5 || j == 4)
+            //        string[] Auto = new string[3];
+            //        if (j == 4 || j == 5)
             //        {
             //            continue;
             //        }
-            //        HtmlNode[] nodes = document.DocumentNode.SelectNodes("//*[@id='th_" + i + "']//td[" + j + "]").ToArray();
-            //        List<string> AutoTemp = new List<string>();
-            //        foreach(var item in nodes)
+            //        HtmlNode[] nodes = document.DocumentNode.SelectNodes("//*[@id='th_" + i + "']/td[" + j + "]").ToArray();
+            //        if (!String.IsNullOrEmpty(nodes[0].InnerText))
             //        {
-            //            AutoTemp.Add(item.InnerText);
+            //            s.Add(nodes[0].InnerText);
             //        }
-            //        AutoList.Add(new Auto {
-            //            Name = AutoTemp[0],
-            //            Manufacturer = "Mersedes",
-            //            BodyType = AutoTemp[1],
-            //            Price = Double.Parse(AutoTemp[2])
-            //        });
             //    }
             //}
+            //int k = 0;
+            //while (k < s.Count / 3)
+            //{
+            //    AutoList.Add(new Auto
+            //    {
+            //        Name = s[k * 3],
+            //        Manufacturer = "Mersedes",
+            //        BodyType = s[k * 3 + 1],
+            //        Price = GetPrice(s[k * 3 + 2])
+            //    });
+            //    k++;
+            //}
+            //return AutoList;
+
             List<Auto> AutoList = new List<Auto>();
             HtmlWeb web = new HtmlWeb();
             HtmlDocument document = web.Load("http://sales.mercedes-avilon.ru/special/goryachee-predlozhenie-table/?csref=avilon_volg:pkw:Mercedes:cpc:Google.Adwords&csref=avilon_volg:pkw:NEW:cpc:Google.Adwords&utm_source=Google.Adwords&utm_medium=cpc&utm_campaign=general_reg&utm_content=ST:g::S:::AP:::PT:1t1::P:1t1::DT:c::RI:1011981::RN:1011981::CI:743824508::GI:42576317487::PI:kwd-16822931::AI:189100468533::KW:mercedes&utm_term=mercedes&gclid=Cj0KEQjwwoLHBRDD0beVheu3lt0BEiQAvU4CKvwRMRFEZMO7i7YYpcleZ9JssEFKzmZeGcp9ZmQIZl8aAq9f8P8HAQ");
             List<string> s = new List<string>();
-            for (int i = 1; i <= 120; i++)
+            int i = 1, iMax = 1000;
+            while (i <= iMax)
             {
                 for (int j = 2; j <= 6; j++)
                 {
@@ -50,12 +60,23 @@ namespace SelectAutoServise.WorkingClasses.Parsers
                     {
                         continue;
                     }
-                    HtmlNode[] nodes = document.DocumentNode.SelectNodes("//*[@id='th_" + i + "']/td[" + j + "]").ToArray();
-                    if (!String.IsNullOrEmpty(nodes[0].InnerText))
+                    HtmlNode[] nodes;
+                    try
                     {
-                        s.Add(nodes[0].InnerText);
+                        nodes = document.DocumentNode.SelectNodes("//*[@id='th_" + i + "']/td[" + j + "]").ToArray();
+                        if (!String.IsNullOrEmpty(nodes[0].InnerText))
+                        {
+                            Console.WriteLine(nodes[0].InnerText);
+                            s.Add(nodes[0].InnerText);
+                        }
                     }
+                    catch (Exception ex)
+                    {
+                        i = iMax;
+                    }
+
                 }
+                i++;
             }
             int k = 0;
             while (k < s.Count / 3)
--- WorkingClasses/Parsers/NissanParser.cs
+++ WorkingClasses/Parsers/NissanParser.cs
@@ -15,13 +15,6 @@ namespace CaseAutoService.WorkingClasses.Parsers
         public List<Auto> GetData()
         {
             List<Auto> AutoList = new List<Auto>();
-            
-            return AutoList;
-        }
-
-        public List<string> ParseNissan()
-        {
-            List<string> AutoSpecification = new List<string>();
             HtmlWeb web = new HtmlWeb();
             HtmlDocument document = web.Load("http://sovaz-auto.ru/new-auto/modelnyj-ryad-nissan/");
             for (int i = 2; i <= 4; i *= 2)
@@ -30,15 +23,98 @@ namespace CaseAutoService.WorkingClasses.Parsers
                 {
                     List<string> AutoTemp = new List<string>();
                     HtmlNode[] nodes = document.DocumentNode.SelectNodes("//*[@id='post-2155']/table/tbody/tr[" + j + "]/th[" + i + "]").ToArray();
-                    foreach (HtmlNode item in nodes)
+                    if(GetPrice(nodes[0].InnerText) != 0)
                     {
-                        AutoSpecification.Add(item.InnerText);
+                        AutoList.Add(new Auto
+                        {
+                            Name = GetName(nodes[0].InnerText),
+                            Manufacturer = "Nissan",
+                            BodyType = GetBodyType(nodes[0].InnerText),
+                            Price = GetPrice(nodes[0].InnerText)
+                        });
+                    }                 
+                }
+            }
+            return AutoList;
+        }
+        private string GetBodyType(string FullString)
+        {
+            string BodyType = "";
+            int StartI = 0;
+            for (int i = 0; i < FullString.Length; i++)
+            {
+                if (FullString[i] == '.')
+                {
+                    StartI = i + 1;
+                    break;
+                }
+            }
+            for (int i = StartI; i < FullString.Length; i++)
+            {
+                BodyType += FullString[i];
+            }
+            return BodyType;
+        }
+        private string GetName(string FullString)
+        {
+            string Name = "";
+            string NameTemp = FullString.Trim();
+            int SpaceValue = 0, StartI = 0, EndI = 0;
+            for (int i = 0; i < NameTemp.Length; i++)
+            {
+                if (NameTemp[i] == ' ')
+                {
+                    SpaceValue++;
+                    if (SpaceValue == 1)
+                    {
+                        StartI = i;
+                    }
+                    if (SpaceValue == 2)
+                    {
+                        EndI = i;
                     }
                 }
             }
-            return AutoSpecification;
-        }
-    }
+            for (int i = StartI + 1; i < EndI; i++)
+            {
 
-    
+                if (NameTemp[i] == 'о' || NameTemp[i] == 'т')
+                {
+                    continue;
+                }
+                Name += NameTemp[i];
+            }
+            return Name;
+        }
+        private double GetPrice(string FullString)
+        {
+            double Price = 0;
+            int StartI = 0;
+            string TempPriceMaster = "";
+            string TempPrice = "";
+            string pattern = @"\b(\d*\W+\d+\W\d+\W+руб)";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(FullString);
+            if (match.Success)
+            {
+                TempPrice = match.Groups[1].Value;
+                for (int i = 0; i < TempPrice.Length; i++)
+                {
+                    if (TempPrice[i] == 'р')
+                    {
+                        StartI = i - 1;
+                    }
+                }
+                for (int i = 0; i < StartI; i++)
+                {
+                    TempPriceMaster += TempPrice[i];
+                }
+                if (!String.IsNullOrEmpty(TempPriceMaster))
+                {
+                    Price = Double.Parse(TempPriceMaster);
+                }
+            }
+            return Price;
+        }   
+    }   
 }
